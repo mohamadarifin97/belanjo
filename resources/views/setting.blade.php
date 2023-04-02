1146cfe2d56c7d264da297a368076040ad0e4534
mohamad arifin <mohamadarifin97.ma50@gmail.com>
2023-04-02 23:15:34 +0800

    commitment

--- resources/views/setting.blade.php
+++ resources/views/setting.blade.php
@@ -14,17 +14,28 @@
                         </div>
                     @endif
 
-                    <div>
-                        <p>Zakat</p>
-                        <p>RM 70</p>
+                    <div class="row mb-3 mt-3">
+                        <div class="col-md-6 ps-5">
+                            <p>Zakat</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>RM 70</p>
+                        </div>
                     </div>
 
                     <form action="{{ route('spending_list.store') }}" method="POST">
                         @csrf
-                        <div class="mb-3">
-                            <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Add commitment">
+                        <div class="row mb-3">
+                            <div class="col-auto">
+                                <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Add commitment">
+                            </div>
+                            <div class="col-auto">
+                                <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Add value">
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-primary btn-sm">Save</button>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Save</button>
                     </form>
                 </div>
             </div>
