61d23fc81e266c22e99c896bcf48dfbb2e95c2c6
mohamad arifin <mohamadarifin97.ma50@gmail.com>
2023-04-25 23:51:25 +0800

    homecontroller api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group( function () {
+    Route::get('home', [HomeController::class, 'index']);
+});
