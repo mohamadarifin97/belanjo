30993e52ab9ef43b9c896540ca641770f381afde
mohamad arifin <mohamadarifin97.ma50@gmail.com>
2023-05-04 16:16:14 +0800

    route auth middleware

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/')->middleware('auth');
 Auth::routes();
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/setting', [SettingController::class, 'index'])->name('setting');
-Route::get('/setting/commitment-list', [SettingController::class, 'listCommitment'])->name('commitment.list');
-Route::post('/setting/commitment-store', [SettingController::class, 'storeCommitment'])->name('commitment.store');
-Route::post('/setting/spend-list-store', [SettingController::class, 'storeSpendingList'])->name('spending_list.store');
+Route::get('/')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+    
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    
+    Route::get('/setting', [SettingController::class, 'index'])->name('setting');
+    Route::get('/setting/commitment-list', [SettingController::class, 'listCommitment'])->name('commitment.list');
+    Route::post('/setting/commitment-store', [SettingController::class, 'storeCommitment'])->name('commitment.store');
+    Route::post('/setting/spend-list-store', [SettingController::class, 'storeSpendingList'])->name('spending_list.store');
+});
