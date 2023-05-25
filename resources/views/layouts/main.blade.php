95b781adafa3c76b53968230933e23fe489e9f5c
mohamad arifin <mohamadarifin97.ma50@gmail.com>
2023-05-25 22:56:32 +0800

    change sidebar and show on hover

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,143 +1,186 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    {{-- BOOTSTRAP --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
-    {{-- BOOTSTRAP ICON --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-    
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+  {{-- BOOTSTRAP --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  {{-- BOOTSTRAP ICON --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+  
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    {{-- DATATABLE --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
-    
-    {{-- TOASTR --}}
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+  {{-- DATATABLE --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 
-    <!-- SCRIPTS -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+  {{-- BOXICON --}}
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+  
+  {{-- TOASTR --}}
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />    
-    @stack('styles')
+  <!-- SCRIPTS -->
+  {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
+  {{-- <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />     --}}
+  <link href="{{ asset('css/styles2.css') }}" rel="stylesheet" />    
+  @stack('styles')
 </head>
-<body>
-    <div id="app">
-        <div class="container-fluid">
-            <div class="row flex-nowrap">
-                <div id="sidebar" class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                            <li class="nav-item">
-                                <a href="{{ route('home') }}" class="nav-link align-middle px-0">
-                                    <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline nav-text">Home</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('setting') }}" class="nav-link align-middle px-0">
-                                    <i class="fs-4 bi-gear"></i> <span class="ms-1 d-none d-sm-inline nav-text">Setting</span>
-                                </a>
-                            </li>
-                            {{-- <li>
-                                <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                                    <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                                <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                    <li class="w-100">
-                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                                    </li>
-                                </ul>
-                            </li> --}}
-                        </ul>
-                        <hr>
-                        <div class="dropdown pb-4">
-                            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                                <span class="d-none d-sm-inline mx-1">{{Auth::user()->username}}</span>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                                <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    Sign out
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col py-3 bg-dark">
-                    @yield('content')
-                </div>
-            </div>
-        </div>
-    </div>
+<body id="body-pd">
+  <header class="header" id="header">
+      <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+  </header>
+  <div class="l-navbar" id="nav-bar">
+      <nav class="nav">
+          <div>
+            <a href="{{ route('home') }}" class="nav_logo">
+              <i class='bx bx-layer nav_logo-icon'></i>
+              <span class="nav_logo-name">BELANJO</span>
+            </a>
+            <div class="nav_list">
+              <a href="{{ route('home') }}" class="nav_link active">
+                <i class='bx bx-grid-alt nav_icon'></i>
+                <span class="nav_name">Home</span>
+              </a>
+              <a href="{{ route('setting') }}" class="nav_link">
+                <i class='bx bx-user nav_icon'></i>
+                <span class="nav_name">Setting</span>
+              </a>
+          </div> 
+          <a href="{{ route('logout') }}" class="nav_link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class='bx bx-log-out nav_icon'></i>
+            <span class="nav_name">SignOut</span>
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+          </form>
+      </nav>
+  </div>
 
-    {{-- JQUERY --}}
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+  <!--Container Main start-->
+  <div class="height-100 bg-light">
+      @yield('content')
+  </div>
+  <!--Container Main end-->
+
+  {{-- JQUERY --}}
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+  <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+  
+  {{-- BOOTSTRAP --}}
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+  
+  {{-- APEXCHART --}}
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+  <script>
+      @if(Session::has('message'))
+          toastr.options =
+          {
+              "closeButton" : true,
+              "progressBar" : true,
+              "positionClass": "toast-top-right",
+          }
+          toastr.success("{{ session('message') }}");
+      @endif
+  
+      @if(Session::has('error'))
+          toastr.options =
+          {
+              "closeButton" : true,
+              "progressBar" : true,
+              "positionClass": "toast-top-right",
+          }
+          toastr.error("{{ session('error') }}");
+      @endif
+  
+      @if(Session::has('info'))
+          toastr.options =
+          {
+              "closeButton" : true,
+              "progressBar" : true,
+              "positionClass": "toast-top-right",
+          }
+          toastr.info("{{ session('info') }}");
+      @endif
+  
+      @if(Session::has('warning'))
+          toastr.options =
+          {
+              "closeButton" : true,
+              "progressBar" : true,
+              "positionClass": "toast-top-right",
+          }
+          toastr.warning("{{ session('warning') }}");
+      @endif
+  </script>
+  <script>
+    const body = document.querySelector('#body-pd');
+    const header = document.querySelector('#header');
+    const navbar = document.querySelector('#nav-bar');
+
+    //show sidebar on hover
+    navbar.addEventListener('mouseenter', function() {
+      navbar.classList.add('show');
+      body.classList.add('body-pd');
+      header.classList.add('body-pd');
+    });
+
+    //close sidebar on mouseleave
+    navbar.addEventListener('mouseleave', function() {
+      navbar.classList.remove('show');
+      body.classList.remove('body-pd');
+      header.classList.remove('body-pd');
+    });
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+
+      const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+      const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId)
+      
+      // Validate that all variables exist
+      if (toggle && nav && bodypd && headerpd) {
+        toggle.addEventListener('click', ()=>{
+        // show navbar
+        nav.classList.toggle('show')
+        // change icon
+        toggle.classList.toggle('bx-x')
+        // add padding to body
+        bodypd.classList.toggle('body-pd')
+        // add padding to header
+        headerpd.classList.toggle('body-pd')
+        })
+      }
+    }
     
-    {{-- BOOTSTRAP --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    showNavbar('header-toggle','nav-bar','body-pd','header')
     
-    {{-- APEXCHART --}}
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    <script>
-        @if(Session::has('message'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true,
-                "positionClass": "toast-top-right",
-            }
-            toastr.success("{{ session('message') }}");
-        @endif
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
     
-        @if(Session::has('error'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true,
-                "positionClass": "toast-top-right",
-            }
-            toastr.error("{{ session('error') }}");
-        @endif
-    
-        @if(Session::has('info'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true,
-                "positionClass": "toast-top-right",
-            }
-            toastr.info("{{ session('info') }}");
-        @endif
-    
-        @if(Session::has('warning'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true,
-                "positionClass": "toast-top-right",
-            }
-            toastr.warning("{{ session('warning') }}");
-        @endif
-    </script>
-    @stack('scripts')
+    function colorLink(){
+      if (linkColor) {
+        linkColor.forEach(l=> l.classList.remove('active'))
+        this.classList.add('active')
+      }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+      // Your code to run since DOM is loaded and ready
+    });
+  </script>
+  @stack('scripts')
 </body>
 </html>
